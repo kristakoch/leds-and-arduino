00555d4d6a99d75808467c273db6c488d9a7e3fe
Krista Koch <k.marie.koch@gmail.com>
2024-12-23 21:05:31 -0600

    Fix names

--- snake-trio.ino
+++ snake-trio.ino
@@ -5,32 +5,36 @@
 
 CRGB leds[NUM_LEDS];
 
+CRGB COLOR_ONE = CRGB::Red;
+CRGB COLOR_TWO = CRGB::Gold;
+CRGB COLOR_THREE = CRGB::Teal;
+
 void setup() {
   FastLED.addLeds<WS2811, LED_PIN, RGB>(leds, NUM_LEDS); // RGB is color order
   FastLED.setBrightness(50); // 0-255
 
-  fill_solid(leds, NUM_LEDS, CRGB::Red);
+  fill_solid(leds, NUM_LEDS, COLOR_ONE);
 }
 
 void loop() {
 
   // Set the string to red bulb by bulb quickly.
   for (int i = 0; i < NUM_LEDS; i+=1) {
-    leds[i] = CRGB::Red;
+    leds[i] = COLOR_ONE;
     FastLED.show();
     delay(25);
   }
   
   // Now do the same for yellow.
   for (int i = 0; i < NUM_LEDS; i+=2) {
-    leds[i] = CRGB::Gold;
+    leds[i] = COLOR_TWO;
     FastLED.show();
     delay(50);
   }
 
   // Now do the same for blue.
   for (int i = 0; i < NUM_LEDS; i+=3) {
-    leds[i] = CRGB::Teal;
+    leds[i] = COLOR_THREE;
     FastLED.show();
     delay(100);
   }
